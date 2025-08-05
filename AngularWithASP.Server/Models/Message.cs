7d40a3d8a3bcd55cbe0702cc06439b40e75e8cba
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-08-05 21:33:48 +0200

    add message class

---
+++ AngularWithASP.Server/Models/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace AngularWithASP.Server.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public required string Text { get; set; }
+        public int Position { get; set; }
+    }
+}
