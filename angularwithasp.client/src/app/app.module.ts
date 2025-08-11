d16ba88852f8a90d97f7566618f792a75e0e07b5
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-08-11 14:54:01 +0200

    Add button and mat label

--- angularwithasp.client/src/app/app.module.ts
+++ angularwithasp.client/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,8 +7,10 @@ import { AppComponent } from './app.component';
 import { ChatComponent } from './pages/chat/chat.component';
 
 import { MatCardModule } from '@angular/material/card';
-import { MatFormField } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -21,8 +23,13 @@ import { MatInput } from '@angular/material/input';
     HttpClientModule,
     AppRoutingModule,
     MatCardModule,
-    MatFormField,
-    MatInput
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularwithasp.client/src/app/pages/chat/chat.component.ts
+++ angularwithasp.client/src/app/pages/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 interface Message {
