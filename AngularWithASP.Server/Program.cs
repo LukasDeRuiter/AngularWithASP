0733c3a13812a2c9b4c322c9f97ac13825b85bfd
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-08-12 23:07:21 +0200

    add context to program file and add setting line

--- AngularWithASP.Server/Program.cs
+++ AngularWithASP.Server/Program.cs
@@ -1,9 +1,16 @@
+using AngularWithASP.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle\
+
+builder.Services.AddDbContext<ChatContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ChatDatabase")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
