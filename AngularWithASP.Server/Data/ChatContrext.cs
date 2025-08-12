2bf56890599db1e0592b528a16116a16b4b1dae2
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-08-12 23:03:48 +0200

    Add db context

---
+++ AngularWithASP.Server/Data/ChatContrext.cs
@@ -0,0 +1,18 @@
+﻿using AngularWithASP.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularWithASP.Server.Data
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ChatContext()
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
