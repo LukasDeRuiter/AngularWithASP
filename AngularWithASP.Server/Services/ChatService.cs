27aff65f91f862b203fe23e097bb65e047032c5a
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-08-17 11:02:20 +0200

    Add getChatMessages

--- AngularWithASP.Server/Services/ChatService.cs
+++ AngularWithASP.Server/Services/ChatService.cs
@@ -1,7 +1,25 @@
-﻿namespace AngularWithASP.Server.Services
+﻿using AngularWithASP.Server.Data;
+using AngularWithASP.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularWithASP.Server.Services
 {
     public class ChatService
     {
+        private readonly ChatContext _chatContext;
 
+        public ChatService(ChatContext chatContext)
+        {
+            _chatContext = chatContext;
+        }
+
+        public List<Message> GetChatMessages()
+        {
+            var messages = _chatContext.Messages
+                .OrderBy(m => m.Position)
+                .ToList();
+
+            return messages;
+        }
     }
 }
