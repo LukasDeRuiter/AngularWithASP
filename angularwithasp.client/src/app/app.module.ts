0945b8bcf19e6aa7f2b22309574c926ac097f14d
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-08-20 23:08:12 +0200

    Update message component path

--- angularwithasp.client/src/app/app.module.ts
+++ angularwithasp.client/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MessageComponent } from './components/message/message/message.component';
+import { MessageComponent } from './components/message/message.component';
 
 
 @NgModule({
